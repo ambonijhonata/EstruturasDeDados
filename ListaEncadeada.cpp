2939bd7212d79b7b8aa85117c660a8c90d161eee
Jhonata Amboni <43749922+ambonijhonata@users.noreply.github.com>
2023-10-06 22:21:05 -0300

    Lista encadeada
    Criada a estrutura de dados e as operações de inicialização, adição de nó ao final, exclusão de determinado nó, limpeza e impressão da lista encadeada simples.

---
+++ ListaEncadeada.cpp
@@ -0,0 +1,122 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* initializeList() {
+    return NULL; 
+}
+
+void addAtEnd(struct Node** list, int data) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    
+    if (node == NULL) {
+        fprintf(stderr, "Erro: Falha na alocação de memória\n");
+        exit(1);
+    }
+    
+    node->data = data;
+    node->next = NULL;
+
+    if (*list == NULL) {
+        *list = node;
+    }
+    else
+    {
+        struct Node* aux = *list;
+        while (aux->next )
+        {
+            aux = aux->next;
+        }
+        aux->next = node;
+    }        
+    
+}
+
+bool updateList(Node** list, int oldData, int newData) {
+    struct Node* current = *list;
+    while (current != NULL) {
+        if (current->data == oldData) {
+            current->data = newData;
+            return true;
+        }
+        current = current->next;
+    }
+    return false;
+}
+
+bool deleteElement(Node** list, int data) {
+    struct Node* before = *list;
+    struct Node* current = *list;
+
+    while (current != NULL) {
+        
+        if (current->data == data) {
+            before->next = current->next;
+            free(current);
+            return true;
+        }
+        before = current;
+        current = current->next;
+    }
+    return false;
+}
+
+void clear(Node** list) {
+    Node* current = *list;
+    Node* nextNode;
+
+    while (current != NULL)
+    {
+        nextNode = current->next;
+        free(current);
+        current = nextNode;
+    }
+
+    *list = NULL;
+}
+
+void printList(Node** list) {    
+    struct Node* current = *list;
+    while (current != NULL) {
+        printf("%d -> ", current->data);
+        current = current->next;
+    }
+
+    *list = NULL;
+}
+
+int main() {
+    
+    Node* list = initializeList();
+
+    addAtEnd(&list, 1);
+    addAtEnd(&list, 2);
+    addAtEnd(&list, 3);
+
+    printList(&list);
+    printf("\n");
+
+    updateList(&list, 2, 4);
+    printList(&list);
+    printf("\n");
+
+    deleteElement(&list, 4);
+    printList(&list);
+    printf("\n");
+    
+    addAtEnd(&list, 5);
+    addAtEnd(&list, 6);
+
+    updateList(&list, 5, 7);
+    printList(&list);
+    printf("\n");
+
+    clear(&list);
+    printList(&list);    
+
+    return 0;
+}
